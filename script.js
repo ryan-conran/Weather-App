e2fc962141683f203e2f17e2deef490d5f71ae08
Ryan Conran <ryan.conran2@gmail.com>
2020-11-04 16:46:59 -0600

    Adjusted weather URL

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let city = "Minneapolis";
 let apiKey = "51f413c7f43e62a941348d7c35e44a31";
-let weatherURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" +apiKey;
+let weatherURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Minneapolis&appid=" + apiKey;
 
 $.ajax({
     url: weatherURL,
     method: "GET"
   }).then(function(response) {
+      console.log(weatherURL);
       console.log(response);
 
   });
